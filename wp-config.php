fdf645c3a043eebd81a619154744813a10d53619
hostingzero <asahkuches@gmail.com>
2023-11-25 18:57:27 -0800

    init

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,5 @@
 <?php
+define( 'WP_CACHE', true );
 /**
  * The base configuration for WordPress
  *
@@ -20,13 +21,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'golfcarts' );
+define( 'DB_NAME', 'u123962300_golf' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u123962300_golfcart' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Talktome@123' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
@@ -85,6 +86,7 @@ define( 'WP_DEBUG', false );
 
 
 
+define( 'FS_METHOD', 'direct' );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
--- wp-content/plugins/wc-multi-currency/template/main.php
+++
@@ -1,2 +0,0 @@
-<?php
-    echo wp_kses_html($output);
